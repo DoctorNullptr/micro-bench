fddc09b76a60f25557bb3388c9f48abaaf4478ce
Nicolas Kleitz <nicolas.kleitz@gmail.com>
2021-10-06 22:56:06 +0200

    add endl benchmark

---
+++ benches/stream_endl.cpp
@@ -0,0 +1,45 @@
+#include <benchmark/benchmark.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+struct TmpFile {
+public:
+    auto stream() const { return std::fstream{path, std::ios_base::out}; }
+
+    ~TmpFile() { fs::remove(path); }
+
+    fs::path path;
+};
+
+void CoutEndl(benchmark::State &state) {
+    TmpFile file{fs::path{"junk.txt"}};
+    {
+        auto stream{file.stream()};
+        for ([[maybe_unused]] auto _: state) {
+            stream << "Hello, world !" << std::endl;
+        }
+    }
+}
+
+BENCHMARK(CoutEndl);
+
+
+void CoutRet(benchmark::State &state) {
+    TmpFile file{fs::path{"junk.txt"}};
+    {
+        auto stream{file.stream()};
+        for ([[maybe_unused]] auto _: state) {
+            stream << "Hello, world !" << '\n';
+        }
+    }
+}
+
+BENCHMARK(CoutRet);
+
+
+
+BENCHMARK_MAIN();
